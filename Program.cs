64348c392de887249607612023ad4e94385df126
Waldo Heudens <waldo@heudens.be>
2023-09-26 11:47:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using WerkenMetRijen;
+
+Rij rij = new Rij();
+char keuze = '0';
+int even;
+do
+{
+    Console.WriteLine("Kies achtereenvolgens:");
+    Console.WriteLine(" 0  Stoppen");
+    Console.WriteLine(" 1  Toevoegen");
+    Console.WriteLine(" 2  Verwijderen");
+    Console.WriteLine(" 3  Toon hele rij");
+    Console.WriteLine(" 4  Maak de rij leeg");
+    Console.Write("Geef je keuze en druk [enter]: ");
+    keuze = Console.ReadLine()[0];
+    Console.WriteLine();
+
+    switch (keuze)
+    {
+        case '0':
+            break;
+        case '1':
+            Console.Write("Geef het toe te voegen getal: ");
+            rij.Toevoegen(Convert.ToInt32(Console.ReadLine()));
+            break;
+        case '2':
+            even = rij.Uithalen();
+            if (even == int.MinValue)
+                Console.WriteLine("De rij is leeg");
+            else
+                Console.WriteLine("Het eerste getal in de rij was " + even);
+            Console.WriteLine();
+            break;
+        case '3':
+            Console.WriteLine("De hele rij is:");
+            Console.Write(rij.ToString());
+            Console.WriteLine();
+            break;
+        case '4':
+            rij.MaakLeeg();
+            break;
+        default:
+            Console.WriteLine("???");
+            break;
+    }
+} while (keuze != '0');
+
+
---
+++ Rij.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WerkenMetRijen
+{
+     internal class Rij
+    {
+        List<int> container;
+
+        internal Rij()
+        {
+            container = new List<int>();
+        }
+
+        public void Toevoegen(int i)
+        {
+            container.Add(i);
+        }
+
+        public int Uithalen()
+        {
+            if (container.Count == 0)
+                return int.MinValue;
+            int even = container[0];
+            container.RemoveAt(0);
+            return even;
+        }
+
+        public void MaakLeeg()
+        {
+            container.Clear();
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            foreach(int i in container) 
+            {
+                s += i.ToString() + "\n";
+            }
+            return s;
+        }
+
+    }
+}
